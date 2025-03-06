c62353dd9403648887d5b1b61262ce46046f3fcc
Tamas Tudor-Ciprian (uig60821) <tudor-ciprian.tamas@continental-corporation.com>
2025-03-06 16:06:41 +0200

    added some structural classes for wallet handling

--- modules_go_here/signer.py
+++ modules_go_here/signer.py
@@ -6,3 +6,8 @@ class signer:
     def __init__(self):
         pass
 
+    def sign_transaction(self):
+        pass
+
+    def verify_signature(self) -> bool:
+        pass
---
+++ modules_go_here/wallet.py
@@ -0,0 +1,11 @@
+import random
+
+
+
+class wallet_manager:
+    def __init__(self,wallet_location:str = None):
+        pass
+
+    def generate_new_wallet(self):
+        pass
+
